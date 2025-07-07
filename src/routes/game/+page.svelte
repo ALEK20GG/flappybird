14fd3711ff5bda43cf7cf979037a98303ca1a8f0
ALEK20GG <alez.lego@gmail.com>
2025-07-07 15:39:35 +0200

    si

---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    //gravity
+    setInterval(() => {
+			console.log("scendo piano piano");
+            console.log("i tubi avanzano");
+		}, 1000);
+    
+</script>
+<div>
+    <img src="../../../static/img/" alt="uccello" class="flex justify-content align-center">
+</div>
