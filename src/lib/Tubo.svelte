6c8deb322898af80cb14eafbfff77b6da704563d
ALEK20GG <alez.lego@gmail.com>
2025-07-08 10:24:51 +0200

    aggiunta tubi (non funziona)

---
+++ src/lib/Tubo.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let x = 100; // percentuale iniziale da destra
+  let h1 = Math.floor(Math.random() * 41) + 10;
+  let h2 = 60 - h1;
+</script>
+
+<div class="absolute top-0" style="left: {x}%; width: 10%; height: 100%;">
+  <div class="h-full flex flex-col justify-between">
+    <img src="/img/tubo.png" alt="tubo" class="w-full h-[{h1}%]" />
+    <img src="/img/tubo_180.png" alt="tubo" style="%;" class="w-full h-[{h2}%]" />
+  </div>
+</div>
--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,11 +1,37 @@
 <script lang="ts">
-    //gravity
-    setInterval(() => {
-			console.log("scendo piano piano");
-            console.log("i tubi avanzano");
-		}, 1000);
-    
+  import { onMount } from 'svelte';
+  import Tubo from '../../lib/Tubo.svelte';
+
+  let tubo_gen_time = 0;
+
+  // Ogni tubo ha x (posizione) e id univoco
+  let tubi: { id: number; x: number }[] = [];
+  let tuboId = 0;
+
+  const tick = () => {
+    tubo_gen_time++;
+
+    // Ogni 10 tick, genera un nuovo tubo (posizione iniziale a destra, 100%)
+    if (tubo_gen_time % 10 === 0) {
+      tubi.push({ id: tuboId++, x: 100 });
+    }
+
+    // Muovi tutti i tubi verso sinistra (es. 1% per tick)
+    tubi = tubi
+      .map(t => ({ ...t, x: t.x - 1 }))
+      .filter(t => t.x + 10 > 0); // rimuove i tubi usciti completamente a sinistra
+
+    requestAnimationFrame(tick);
+  };
+
+  onMount(() => {
+    requestAnimationFrame(tick);
+  });
 </script>
-<div>
-    <img src="../../../static/img/" alt="uccello" class="flex justify-content align-center">
+
+<div class="relative w-full h-screen overflow-hidden bg-sky-300">
+  {#each tubi as tubo (tubo.id)}
+    <Tubo x={tubo.x} />
+  {/each}
 </div>
+
